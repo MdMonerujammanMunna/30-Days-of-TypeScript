e05d0364e276a945a6c95c115c09814bd7123275
Md.Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-07-26 11:29:45 +0600

    Day-7 Revision Practice Questions part-2

--- Day-7/Revision Practice Questions/TypevsInterface.ts
+++ Day-7/Revision Practice Questions/TypevsInterface.ts
@@ -2,13 +2,87 @@
 
 // - `type` ব্যবহার করে একটি Student তৈরি করো।
 // Answer----=>
+type Student = {
+    Name: string,
+    Roll: number,
+    Age: number,
+    Class: number,
+    School: string
+}
+
+const SutdentOne: Student = {
+    Name: "Munna",
+    Roll: 89,
+    Age: 9,
+    Class: 8,
+    School: "x school and collage"
+}
+console.log(SutdentOne)
 // - `interface` ব্যবহার করে একটি Student তৈরি করো।
 // Answer----=>
+interface StudentInto {
+    Name: string,
+    Roll: number,
+    Age: number,
+    Class: number,
+    School: string
+}
+
+const SutdentTwo: StudentInto = {
+    Name: "Roy",
+    Roll: 9,
+    Age: 21,
+    Class: 15,
+    School: "y school and collage"
+}
+console.log(SutdentTwo)
 // - `type` ব্যবহার করে একটি Product তৈরি করো।
 // Answer----=>
+type Products = {
+    id: number | string,
+    Name: string,
+    Brand: string,
+    Price: number,
+}
+
+const ProductOne: Products = {
+    id: 6654,
+    Name: "Mobile Phone",
+    Brand: "Realme",
+    Price: 90000
+}
+console.log(ProductOne)
 // - `interface` ব্যবহার করে একটি Product তৈরি করো।
 // Answer----=>
+interface ProductsInto {
+    id: number | string,
+    Name: string,
+    Brand: string,
+    Price: number,
+}
+
+const ProductTwo: ProductsInto = {
+    id: 25,
+    Name: "Air buts",
+    Brand: "Apple",
+    Price: 150000
+}
+console.log(ProductTwo)
 // - একটি User Interface তৈরি করো যেখানে একটি Optional Property থাকবে।
 // Answer----=>
+interface UserValue {
+    Name: string,
+    Age: number,
+    IsStudent: boolean,
+    Number?: number,
+}
+
+const OneMan: UserValue = {
+    Name: "Munna",
+    Age: 21,
+    IsStudent: true,
+    Number: 12532566223  //commit it , it not given error becourse it optional property
+}
+console.log(OneMan)
 
 // ---
--- Day-7/Revision Practice Questions/UnionTypesLiteralTypes.ts
+++ Day-7/Revision Practice Questions/UnionTypesLiteralTypes.ts
@@ -1,15 +1,33 @@
 
 // ## 5. Union Types & Literal Types
- 
+
 // - এমন একটি Variable তৈরি করো যেখানে Number অথবা String রাখা যাবে।
 // Answer----=>
+const NumStr: number | string = 67 //"Munna" 
+console.log(NumStr)
 // - এমন একটি Variable তৈরি করো যেখানে `"success"` অথবা `"error"` রাখা যাবে।
 // Answer----=>
+type Valuo = "success" | "error"
+
+const SucEro: Valuo = "error"
+console.log(SucEro)
 // - এমন একটি Variable তৈরি করো যেখানে `"admin"` অথবা `"user"` রাখা যাবে।
 // Answer----=>
+type Role = "Admin" | "User"
+
+const Who: Role = "Admin"
+console.log(Who)
 // - এমন একটি Variable তৈরি করো যেখানে `"male"` অথবা `"female"` রাখা যাবে।
 // Answer----=>
+type Gender = "male" | "female"
+
+const Sex: Gender = "male"
+console.log(Sex)
 // - এমন একটি Variable তৈরি করো যেখানে Number অথবা Boolean রাখা যাবে।
 // Answer----=>
 
+type BoolNum = number | Boolean
+
+const Numboo: BoolNum = 38 //true
+console.log(Numboo)
 // ---
